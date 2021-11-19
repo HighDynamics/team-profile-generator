c950cb05dd7734ed36bbb310b6b4797279da86fe
Daniel Denton <danielryandenton@gmail.com>
2021-11-19 14:28:55 -0600

    add css; add icons

--- src/html-template.js
+++ src/html-template.js
@@ -3,10 +3,10 @@ const generateManager = (manager) => {
     <article class='card'>
         <div class='card-header'>
             <h2 class='name'>${manager.getName()}</h2>
-            <h3 class='role'>${manager.getRole()}</h3>
+            <h3 class='role'><i class="fas fa-mug-hot"></i> ${manager.getRole()}</h3>
         </div>
         <div class='details'>
-            <p>id: ${manager.getId()}</p>
+            <p>ID: ${manager.getId()}</p>
             <p>Email: 
             <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
             </p> 
@@ -24,16 +24,16 @@ const generateEngineers = (engineers) => {
     <article class='card'>
         <div class='card-header'>
             <h2 class='name'>${engineer.getName()}</h2>
-            <h3 class='role'>${engineer.getRole()}</h3>
+            <h3 class='role'><i class="fas fa-glasses"></i> ${engineer.getRole()}</h3>
         </div>
         <div class='details'>
-            <p>id: ${engineer.getId()}</p>
+            <p>ID: ${engineer.getId()}</p>
             <p>Email: 
                 <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
             </p> 
             <p>GitHub: <a href="https://github.com/${
               engineer.github
-            } target="_blank">${engineer.github}</a></p>
+            }" target="_blank">${engineer.github}</a></p>
         </div>
     </article>
         `;
@@ -50,10 +50,10 @@ const generateInterns = (interns) => {
         <article class='card'>
             <div class='card-header'>
                 <h2 class='name'>${intern.getName()}</h2>
-                <h3 class='role'>${intern.getRole()}</h3>
+                <h3 class='role'><i class="fas fa-user-graduate"></i> ${intern.getRole()}</h3>
             </div>
             <div class='details'>
-                <p>id: ${intern.getId()}</p>
+                <p>ID: ${intern.getId()}</p>
                 <p>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p> 
                 <p>School: ${intern.school}</p>
             </div>
@@ -73,6 +73,7 @@ const generateHtml = ({ manager, engineers, interns }) => {
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel='stylesheet' href='style.css'>
+      <script src="https://kit.fontawesome.com/fe54bea947.js" crossorigin="anonymous"></script>
       <title>My Team</title>
   </head>
   <body>
